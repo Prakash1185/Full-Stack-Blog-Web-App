31696043d9cf80c56549f2d8152d36bc5696fc63
Prakash Kumar <pgcet601@gmail.com>
2025-07-08 14:56:39 +0530

    feat:ui update

--- app/layout.js
+++ app/layout.js
@@ -1,34 +1,32 @@
+"use client";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { usePathname } from "next/navigation";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "Wryto",
-  description: "Full stack markdown based b.og app",
-};
-
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const isAdminRoute = pathname?.startsWith("/admin");
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${dmSans.className}`}
-      >
+      <body className={`${dmSans.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar/>
+          {!isAdminRoute && <Navbar />}
           {children}
-          <Footer/>
+          {!isAdminRoute && <Footer />}
         </ThemeProvider>
       </body>
     </html>
